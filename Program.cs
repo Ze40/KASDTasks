969726b53a9ca9c3d53fb71ca623ae1011c5be41
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-09-05 20:11:43 +0300

    Update Program.cs
    Добавил коментарии в код

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿string filePath = "D://Projects//Алгоритмы//05-09-2024//05-09-2024//data.txt";
+//Создаем необходиые перемменные: путь к файлу, строку в которую будем считывать, размерность пр-ва
+string filePath = "D://Projects//Алгоритмы//05-09-2024//05-09-2024//data.txt";
 string? line;
-
 int n;
 
+//Метод для проверки на симетричность
 bool IsSimetric(int[][] matrix)
 {
     for (int i = 0; i < matrix.Length; i++)
@@ -15,11 +16,14 @@ bool IsSimetric(int[][] matrix)
     return true;
 }
 
+//Метод для вычесления длины вектора через умножении вектора на матрицу
 double MatrixMultiplyVector(int[][] matrix, int[] vector)
 {
     int[] newMatrix;
     int sum = 0;
     newMatrix = new int[matrix.Length];
+
+    //Умножаем вектор на матрицу
     for (int i = 0; (i < matrix.Length); i++)
     {
         sum = 0;
@@ -29,24 +33,32 @@ double MatrixMultiplyVector(int[][] matrix, int[] vector)
         }
         newMatrix[i] = sum;
     }
+
+    //Умножаем получившееся на транспонированный вектор
     sum = 0;
     for(int i = 0; i < vector.Length; i++)
     {
         sum += newMatrix[i] * vector[i];
     }
+
+    //Возвращаем корень из числа 
     return Math.Sqrt(sum);
 }
 
 try
 {
+    //Создаем поток для чтения файла
     StreamReader sr = new StreamReader(filePath);
 
+    //Считываем размерность
     n = Convert.ToInt32(sr.ReadLine());
+    
+    //Создаем матрицу пространства
     int[][] matrix = new int[n][];
-
     for (int i = 0; i < n; i++)
     {
         line = sr.ReadLine();
+        //проверка на пустую строку
         if (line == null) {
             Console.WriteLine("ERR");
             break;
@@ -56,6 +68,7 @@ try
         matrix[i] = a;
     }
 
+    //Проверка на симетричность
     if (IsSimetric(matrix) != true)
     {
         Console.WriteLine("not Sometric");
@@ -63,6 +76,7 @@ try
         return;
     }
 
+    //Считывание вектора
     line = sr.ReadLine();
     if (line == null)
     {
@@ -72,9 +86,12 @@ try
     }
     int[] vector = line.Split(" ").Select(x => Convert.ToInt32(x)).ToArray();
 
+
+    //Нахождение длины и вывод ответа
     double ans = MatrixMultiplyVector(matrix, vector);
     Console.WriteLine("Answer: " + ans);
 
+    //Закрытие файла
     sr.Close();
 }catch(Exception e)
 {
