32a07d000d23d3988490eb065ab214382599717d
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-10-10 13:27:17 +0300

    Add Tasks 13 and 14

---
+++ Task13/Task13/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SortingLibrary;
+using GenerationLibrary;
+using System.IO;
+using System.Reflection;
+
+namespace Task13
+{
+    public partial class Form1 : Form
+    {
+        public void SetPath()
+        {
+            string appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            pathToArray = appDir + @"\array.txt";
+            pathToTime = appDir + @"\time.txt";
+        }
+        string pathToArray;
+        string pathToTime;
+
+        int selectedGroupIndex = -1;
+        int selectedArrayTypeIndex = -1;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedGroupIndex = GroupComboBox.SelectedIndex;
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedArrayTypeIndex = ArrayTypeComboBox.SelectedIndex;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyLib;
+
+namespace Task14
+{
+    static class Project
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 65, 7};
+            MyDequeue<int> dequeue = new MyDequeue<int>();
+            Console.WriteLine(dequeue.PeekFirst());
+            dequeue.Add(123, 2);
+            dequeue.Print();
+            dequeue.Remove(1, 65, 123);
+            dequeue.Print();
+            dequeue.Retain(2, 2);
+            dequeue.Print();
+            Console.WriteLine(dequeue.Contains(65, 4));
+            Console.WriteLine(dequeue.Size());
+        }
+    }
+}
