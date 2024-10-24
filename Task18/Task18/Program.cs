0712187d469582f9a889ea099367703c5c4a9e1c
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-10-24 13:22:07 +0300

    Task 18

---
+++ Task18/Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyLib;
+
+namespace Task18
+{
+    static class Project
+    {
+
+        static void Main(string[] args)
+        {
+            MyHashMap<int, int> map = new MyHashMap<int, int>();
+            map.Push(1, 1);
+            map.Push(2, 2);
+            map.Push(3, 3);
+            map.Push(4, 4);
+            map.Push(5, 5);
+            map.Push(3, 6);
+            int[] keys = map.KeySet();
+            (int, int)[] arr = map.EntrySet();
+            Console.WriteLine(map.ContainKey(6));
+        }
+    }
+}
