7e0e4605f6731a1c68698542a06b9f9cf230b8f1
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-10-24 13:18:00 +0300

    Add Tasks 15, 16

---
+++ Task15/Task15/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using MyLib;
+
+namespace Task15
+{
+    static class Project
+    {
+        public static string SetPath(string name)
+        {
+            string appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(appDir, name);
+        }
+
+        static int CountOfDigital(string str) {
+            int count = 0;
+            char[] digitals = new char[] {'0','1','2','3','4','5','6','7','8','9'};
+            foreach (char symbol in str) if (digitals.Contains(symbol)) count++;
+            return count;
+        }
+        static int CountOfSpace(string str)
+        {
+            int count = 0;
+            foreach (char symbol in str) if (symbol == ' ') count++;
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            MyDequeue<string> dequeue = new MyDequeue<string>();
+            try
+            {
+                StreamReader sr = new StreamReader(SetPath("input.txt"));
+                string? line = sr.ReadLine();
+                if (line != null) { dequeue.Add(line); }
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    if ( line != null )
+                    {
+                        if (CountOfDigital(line) > CountOfDigital(dequeue.GetFirst())) dequeue.AddLast(line);
+                        else dequeue.AddFirst(line);
+                    }
+                }
+                sr.Close();
+
+                StreamWriter sw = new StreamWriter(SetPath("sorted.txt"));
+                for (int i = 0; i < dequeue.Size(); i++)
+                {
+                    sw.WriteLine(dequeue.Get(i));
+                }
+                sw.Close();
+            }catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+            int N =Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < dequeue.Size() ; i++)
+            {
+                if (CountOfSpace(dequeue.Get(i)) > N) dequeue.Remove(index: i);
+            }
+            for (int i = 0;i < dequeue.Size() ; i++) Console.WriteLine(dequeue.Get(i));
+        }
+    }
+}
---
+++ Task16/Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLib;
+
+namespace Task16
+{
+    static class Project
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {1,2,3,4,5,6,7,5};
+            MyLinkedList<int> list = new MyLinkedList<int>(arr);
+            Console.WriteLine(list.RemoveLastOccurrence(5));
+        }
+    }
+}
