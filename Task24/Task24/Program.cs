9f5567d51e8c5ad21218769e30e64cc0332db549
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-11-19 14:56:23 +0300

    Add Task24

---
+++ Task24/Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyLib;
+
+namespace Task24
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyTreeSet<int> tree = new MyTreeSet<int>(new int[] {1,2,3,4,5,6,7, 34,8});
+            int[] arr = tree.ToArray();
+            foreach (int i in tree) Console.Write(i + " ");
+
+        }
+    }
+}
