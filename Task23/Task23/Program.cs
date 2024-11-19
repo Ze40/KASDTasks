171f767d567a377f494f981735c3d5c730e76424
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-11-19 14:55:46 +0300

    Add Task23

---
+++ Task23/Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyLib;
+
+namespace Task23
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyHashSet<int> set = new MyHashSet<int>(new int[] { 1,3,5,7,9 });
+            MyHashSet<int> set2 = new MyHashSet<int>(new int[] { 1,2, 4, 6, 8, 0 });
+
+            MyHashSet<int> subset = set.SubSet(2, 6);
+            MyHashSet<int> inter = set.Intersection(set2);
+            string str = set.ToString();
+            Console.WriteLine(set.EqualsSet(set2));
+        }
+    }
+}
