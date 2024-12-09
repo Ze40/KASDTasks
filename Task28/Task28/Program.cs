6bb85cb34eab408946ffd9eb6548fc21396cd030
Ze40 <110056449+Ze40@users.noreply.github.com>
2024-12-09 15:14:23 +0300

    Add Task 28

---
+++ Task28/Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿using MyLib;
+
+namespace Task28
+{
+    class Project
+    {
+
+        static void Main(string[] args)
+        {
+            MyTreeSet<int> ints = new MyTreeSet<int>(new int[] {13, 8, 17, 1, 11, 15, 25, 6, 22, 27});
+
+            IMyIterator<int> it = ints.GetMyItr();
+
+            while (it.HasNext())
+            {
+                Console.WriteLine(it.Current());
+                it.Next();
+            }
+
+            Console.WriteLine("=====");
+            foreach (int i in ints) Console.WriteLine(i);
+        }
+    }
+}
